625a355eb93974da9d3a813e5d83f3c6bbfd8809
YongliuLi <ylli@thoughtworks.com>
2016-12-13 17:00:35 +0800

    [#0] complete all tests

--- src/main/java/com/laoniu/RomanNumerals.java
+++ src/main/java/com/laoniu/RomanNumerals.java
@@ -3,16 +3,73 @@ package com.laoniu;
 public class RomanNumerals {
     public static String convert(int number) {
         StringBuilder sb = new StringBuilder();
+        getPos1000(number / 1000, sb);
+        getPos100((number % 1000) / 100, sb);
+        getPos10((number % 100) / 10, sb);
+        getPos1(number % 10, sb);
+        return sb.toString();
+    }
+
+    private static void getPos1000(int number, StringBuilder sb) {
+        if (number <= 4) {
+            for (int i = 0; i < number; i++) {
+                sb.append("M");
+            }
+        }
+    }
+
+    private static void getPos100(int number, StringBuilder sb) {
+        if (number < 4) {
+            for (int i = 0; i < number; i++) {
+                sb.append("C");
+            }
+        }
+        if (number == 4) {
+            sb.append("CD");
+        }
+        if (number == 9) {
+            sb.append("CM");
+        }
+        if (number < 9 && number >= 5) {
+            sb.append("D");
+            for (int i = 0; i < number - 5; i++) {
+                sb.append("C");
+            }
+        }
+    }
+
+
+    private static void getPos10(int number, StringBuilder sb) {
+        if (number < 4) {
+            for (int i = 0; i < number; i++) {
+                sb.append("X");
+            }
+        }
+        if (number == 4) {
+            sb.append("XL");
+        }
+        if (number == 9) {
+            sb.append("XC");
+        }
+        if (number < 9 && number >= 5) {
+            sb.append("L");
+            for (int i = 0; i < number - 5; i++) {
+                sb.append("X");
+            }
+        }
+    }
+
+    private static void getPos1(int number, StringBuilder sb) {
         if (number < 4) {
             for (int i = 0; i < number; i++) {
                 sb.append("I");
             }
         }
         if (number == 4) {
-            return "IV";
+            sb.append("IV");
         }
         if (number == 9) {
-            return "IX";
+            sb.append("IX");
         }
         if (number < 9 && number >= 5) {
             sb.append("V");
@@ -20,6 +77,7 @@ public class RomanNumerals {
                 sb.append("I");
             }
         }
-        return sb.toString();
     }
+
+
 }
--- src/test/java/com/laoniu/TestRomanNumerals.java
+++ src/test/java/com/laoniu/TestRomanNumerals.java
@@ -7,28 +7,59 @@ import static junit.framework.TestCase.assertEquals;
 public class TestRomanNumerals {
     @Test
     public void should_1_to_9_correct() {
-        assertEquals("I",RomanNumerals.convert(1));
-        assertEquals("II",RomanNumerals.convert(2));
-        assertEquals("III",RomanNumerals.convert(3));
-        assertEquals("IV",RomanNumerals.convert(4));
-        assertEquals("V",RomanNumerals.convert(5));
-        assertEquals("VI",RomanNumerals.convert(6));
-        assertEquals("VII",RomanNumerals.convert(7));
-        assertEquals("VIII",RomanNumerals.convert(8));
-        assertEquals("IX",RomanNumerals.convert(9));
+        assertEquals("I", RomanNumerals.convert(1));
+        assertEquals("II", RomanNumerals.convert(2));
+        assertEquals("III", RomanNumerals.convert(3));
+        assertEquals("IV", RomanNumerals.convert(4));
+        assertEquals("V", RomanNumerals.convert(5));
+        assertEquals("VI", RomanNumerals.convert(6));
+        assertEquals("VII", RomanNumerals.convert(7));
+        assertEquals("VIII", RomanNumerals.convert(8));
+        assertEquals("IX", RomanNumerals.convert(9));
     }
 
     @Test
     public void should_10_to_90_correct() {
-//        assertEquals("X",RomanNumerals.convert(10));
-//        assertEquals("II",RomanNumerals.convert(2));
-//        assertEquals("III",RomanNumerals.convert(3));
-//        assertEquals("IV",RomanNumerals.convert(4));
-//        assertEquals("V",RomanNumerals.convert(5));
-//        assertEquals("VI",RomanNumerals.convert(6));
-//        assertEquals("VII",RomanNumerals.convert(7));
-//        assertEquals("VIII",RomanNumerals.convert(8));
-//        assertEquals("IV",RomanNumerals.convert(9));
+        assertEquals("X", RomanNumerals.convert(10));
+        assertEquals("XX", RomanNumerals.convert(20));
+        assertEquals("XXX", RomanNumerals.convert(30));
+        assertEquals("XL", RomanNumerals.convert(40));
+        assertEquals("L", RomanNumerals.convert(50));
+        assertEquals("LX", RomanNumerals.convert(60));
+        assertEquals("LXX", RomanNumerals.convert(70));
+        assertEquals("LXXX", RomanNumerals.convert(80));
+        assertEquals("XC", RomanNumerals.convert(90));
     }
+
+
+    @Test
+    public void should_100_to_900_correct() {
+        assertEquals("C", RomanNumerals.convert(100));
+        assertEquals("CC", RomanNumerals.convert(200));
+        assertEquals("CCC", RomanNumerals.convert(300));
+        assertEquals("CD", RomanNumerals.convert(400));
+        assertEquals("D", RomanNumerals.convert(500));
+        assertEquals("DC", RomanNumerals.convert(600));
+        assertEquals("DCC", RomanNumerals.convert(700));
+        assertEquals("DCCC", RomanNumerals.convert(800));
+        assertEquals("CM", RomanNumerals.convert(900));
+    }
+
+    @Test
+    public void should_1000_to_4000_correct() {
+        assertEquals("M", RomanNumerals.convert(1000));
+        assertEquals("MM", RomanNumerals.convert(2000));
+        assertEquals("MMM", RomanNumerals.convert(3000));
+        assertEquals("MMMM", RomanNumerals.convert(4000));
+    }
+
+    @Test
+    public void test_others() {
+        assertEquals("MCMXC", RomanNumerals.convert(1990));
+        assertEquals("MMVIII", RomanNumerals.convert(2008));
+        assertEquals("XCIX", RomanNumerals.convert(99));
+        assertEquals("XLVII", RomanNumerals.convert(47));
+    }
+
 
 }

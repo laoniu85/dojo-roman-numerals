e954a74c656a0060759887f2f61f41caa212d10a
YongliuLi <ylli@thoughtworks.com>
2016-12-13 16:42:05 +0800

    [#0] fisrt version of roman numerals

--- src/main/java/com/laoniu/RomanNumerals.java
+++ src/main/java/com/laoniu/RomanNumerals.java
@@ -1,10 +1,25 @@
 package com.laoniu;
 
 public class RomanNumerals {
-    public static String convert(int number){
-        if(number==1){
-            return "I";
+    public static String convert(int number) {
+        StringBuilder sb = new StringBuilder();
+        if (number < 4) {
+            for (int i = 0; i < number; i++) {
+                sb.append("I");
+            }
         }
-        return "";
+        if (number == 4) {
+            return "IV";
+        }
+        if (number == 9) {
+            return "IX";
+        }
+        if (number < 9 && number >= 5) {
+            sb.append("V");
+            for (int i = 0; i < number - 5; i++) {
+                sb.append("I");
+            }
+        }
+        return sb.toString();
     }
 }
--- src/test/java/com/laoniu/TestRomanNumerals.java
+++ src/test/java/com/laoniu/TestRomanNumerals.java
@@ -6,40 +6,29 @@ import static junit.framework.TestCase.assertEquals;
 
 public class TestRomanNumerals {
     @Test
-    public void should_1_correct() {
+    public void should_1_to_9_correct() {
         assertEquals("I",RomanNumerals.convert(1));
+        assertEquals("II",RomanNumerals.convert(2));
+        assertEquals("III",RomanNumerals.convert(3));
+        assertEquals("IV",RomanNumerals.convert(4));
+        assertEquals("V",RomanNumerals.convert(5));
+        assertEquals("VI",RomanNumerals.convert(6));
+        assertEquals("VII",RomanNumerals.convert(7));
+        assertEquals("VIII",RomanNumerals.convert(8));
+        assertEquals("IX",RomanNumerals.convert(9));
     }
+
     @Test
-    public void should_2_correct() {
-        assertEquals("I",RomanNumerals.convert(2));
-    }
-    @Test
-    public void should_3_correct() {
-        assertEquals("I",RomanNumerals.convert(3));
-    }
-    @Test
-    public void should_4_correct() {
-        assertEquals("I",RomanNumerals.convert(4));
-    }
-    @Test
-    public void should_5_correct() {
-        assertEquals("I",RomanNumerals.convert(5));
-    }
-    @Test
-    public void should_6_correct() {
-        assertEquals("I",RomanNumerals.convert(6));
-    }
-    @Test
-    public void should_7_correct() {
-        assertEquals("I",RomanNumerals.convert(7));
-    }
-    @Test
-    public void should_8_correct() {
-        assertEquals("I",RomanNumerals.convert(8));
-    }
-    @Test
-    public void should_9_correct() {
-        assertEquals("I",RomanNumerals.convert(9));
+    public void should_10_to_90_correct() {
+//        assertEquals("X",RomanNumerals.convert(10));
+//        assertEquals("II",RomanNumerals.convert(2));
+//        assertEquals("III",RomanNumerals.convert(3));
+//        assertEquals("IV",RomanNumerals.convert(4));
+//        assertEquals("V",RomanNumerals.convert(5));
+//        assertEquals("VI",RomanNumerals.convert(6));
+//        assertEquals("VII",RomanNumerals.convert(7));
+//        assertEquals("VIII",RomanNumerals.convert(8));
+//        assertEquals("IV",RomanNumerals.convert(9));
     }
 
 }

04c0daedc3143fb2175f029f46e90782ced8445f
YongliuLi <ylli@thoughtworks.com>
2016-12-13 17:36:04 +0800

    [#0] first reverse roman number

--- src/main/java/com/laoniu/ReverseRomanNumerals.java
+++ src/main/java/com/laoniu/ReverseRomanNumerals.java
@@ -1,4 +1,9 @@
 package com.laoniu;
 
+import static com.laoniu.RomanNumber.*;
+
 public class ReverseRomanNumerals {
+    public static int convert(String numberString) {
+        return 1;
+    }
 }
